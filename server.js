04dcc7572881293578d7696476d0956afd83795a
iammanikandanp <manisunny632@gmail.com>
2026-04-04 03:51:06 +0530

    server fix

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:5000"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost:5000",
+    /\.vercel\.app$/,
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: false,
